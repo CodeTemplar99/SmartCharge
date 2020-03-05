7a37caf6bc04f7202955cc8c1e4779ce9cd91dbd
Chiadika <udoagwac@gmail.com>
2020-03-06 00:11:56 +0100

    2nd commit with different pages added

--- js/spine.js
+++ js/spine.js
@@ -1,25 +1,29 @@
-// explore buttons scroll
-var exploreButtons = document.querySelectorAll('.explore');
-exploreButtons.forEach(function (i) {
-    i.addEventListener('click', endScroll)
-    function endScroll() {
-        window.scrollTo({
-            top: 5300,
-            // left: 100,
-            behavior: 'smooth'
-        });
-        // }
-    }
-})
+// // explore buttons scroll
+// var exploreButtons = document.querySelectorAll('.explore');
+// exploreButtons.forEach(function (i) {
+//     i.addEventListener('click', endScroll)
+//     function endScroll() {
+//         window.scrollTo({
+//             top: 5300,
+//             // left: 100,
+//             behavior: 'smooth'
+//         });
+//         // }
+//     }
+// })
 
 // See more scroll
 
 
-document.querySelector('.learn-more').addEventListener('click', function () {
-    window.scrollTo({
-        top: 710,
-        behavior: 'smooth'
-    });
+// document.querySelector('#tab2').addEventListener('click', function () {
+//     window.scrollTo({
+//         top: 710,
+//         behavior: 'smooth'
+//     });
+// })
+
+
+document.querySelector('#tab2-link').addEventListener('click', function () {
+    document.querySelector('#tab1').style.display = "none";
+    document.querySelector('#tab2').style.display = "block";
 })
-
-
