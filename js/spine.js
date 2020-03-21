0a2624aef74a7df7ea4c43f7f6e2e80120e9b362
Chiadika <udoagwac@gmail.com>
2020-03-21 19:17:47 +0100

    fixed all major UI

--- js/spine.js
+++ js/spine.js
@@ -1,3 +1,16 @@
-// $('#myCollapsible').collapse({
-//     toggle: true
-// })
+
+let isOpen = false
+
+function showBoard() {
+    document.querySelector('.hover-board').style.display = "block";
+}
+
+function hideBoard() {
+    document.querySelector('.hover-board').style.display = "none";
+}
+
+function showBar() {
+    document.querySelector('#navbarSupportedContent').style.display = isOpen ? "none" : "block"
+    isOpen = !isOpen
+
+}
